a8e31df7b2148ee49ab297ebc3c5c5f78a7796b0
zhutwo <tech@iMac-23.local>
2019-04-08 16:11:26 -0400

    port song.swift code

--- MoreCowbell/Conductor/Song.swift
+++ MoreCowbell/Conductor/Song.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import SpriteKit
+
+public class Song {
+    
+    var bars:[Bar] = [Bar]()
+    var numBars = 0;
+    var totalNotes = 0;
+    
+    var beat = 0.0;
+    var bpm = 0.0;
+    var timeSigTop = 0.0;
+    var timeSigBottom = 0.0;
+    var startOffset = 0.0;
+    
+    var musicTrack:SKAudioNode!
+    
+    init(numBars:Int, bpm:Double, timeSigTop:Double, timeSigBottom:Double, startOffset:Double, filePath:String) {
+        let music = SKAudioNode(fileNamed: filePath)
+        musicTrack = music
+        
+        self.totalNotes = 0;
+        self.numBars = numBars;
+        self.bpm = bpm;
+        self.timeSigTop = timeSigTop;
+        self.timeSigBottom = timeSigBottom;
+        self.startOffset = startOffset;
+        //self.bars = new Bar[numBars];
+        self.beat = 60.0 / bpm;
+    }
+}
