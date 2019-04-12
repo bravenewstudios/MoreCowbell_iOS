3b16dac5e2a830a51dc0dbec8abe962d3462756c
Tech <tech@iMac-2.local>
2019-04-12 17:21:33 -0400

    add base scene class

---
+++ MoreCowbell/Screens/BaseScene.swift
@@ -0,0 +1,23 @@
+//
+//  BaseScene.swift
+//  MoreCowbell
+//
+//  Created by Tech on 2019-04-12.
+//  Copyright © 2019 BraveNewStudios. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class BaseScene: SKScene {
+    
+    var gameInstance:GameInstance!
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+    }
+    
+    func SetGameInstance(inst:GameInstance) {
+        gameInstance = inst
+    }
+}
